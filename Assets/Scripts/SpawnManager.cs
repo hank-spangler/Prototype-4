b25a465bd624d0d7e479f40aadf32724d38ef73d
hank-spangler <56128553+hank-spangler@users.noreply.github.com>
2020-02-27 12:30:09 -0800

    different changes

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    private float spawnRange = 9;
+    private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemywave(3);
+        SpawnEnemywave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+        
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemywave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 
-void SpawnEnemywave(int enemiestoSpawn)
+    void SpawnEnemywave(int enemiesToSpawn)
 {
-    for (int i = 0; i < enemiestoSpawn; i++)
+    for (int i = 0; i < enemiesToSpawn; i++)
     {
         Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
